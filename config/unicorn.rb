e7126304867695a469c24701aea7afb5fea24594
Brett Hardin <bhardin@kissmetrics.com>
2014-12-24 17:39:20 -0800

    Fixing unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,16 @@
 # Set the working application directory
 APP_PATH = "/var/www/bgg-tools/current"
+SHARED_PATH = "/var/www/bgg-tools/shared"
 working_directory APP_PATH
 
 # Unicorn PID file location
-pid APP_PATH + "/tmp/pids/unicorn.pid"
+pid SHARED_PATH + "/pids/unicorn.pid"
 
 # Path to logs
 stderr_path APP_PATH + "/log/unicorn.log"
 stdout_path APP_PATH + "/log/unicorn.log"
 
 # Unicorn socket
-listen "/tmp/unicorn.bgg-tools.sock"
 listen "/tmp/unicorn.bgg-tools.sock"
 
 # Number of processes
