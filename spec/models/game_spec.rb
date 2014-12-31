6e6adebc8b37a80b1a6da13b8e6873349bbcad45
Brett Hardin <bhardin@kissmetrics.com>
2014-12-31 15:17:16 -0800

    Adding tests for #needs_updating?

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,6 +1,30 @@
 require 'spec_helper'
 
 describe Game do
+  let(:game) { Game.new(name: "acquire", bgg_id: 5) }
+  describe '#needs_updating?' do
+    context "when game has no name" do
+      it 'is true' do
+        game.stub(:name).and_return(nil)
+        expect(game.needs_updating?).to be_true
+      end
+    end
+
+    context "when game hasn't been updated in 1 month" do
+      it 'is true' do
+        game.stub(:updated_at).and_return(Time.zone.now - 1.month - 1.second)
+        expect(game.needs_updating?).to be_true
+      end
+    end
+
+    context "when game hasn't been updated in 2 weeks" do
+      it 'is false' do
+        game.stub(:updated_at).and_return(Time.zone.now - 2.weeks)
+        expect(game.needs_updating?).to be_false
+      end
+    end
+  end
+
   describe '#update_stuff' do
     context "when game has no bgg_id" do
       it "raises an error" do
