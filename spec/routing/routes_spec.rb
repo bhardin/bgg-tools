f993872749a6049e4ab687f4f08ccc1e24adb695
Brett Hardin <bhardin@kissmetrics.com>
2015-01-23 15:06:13 -0800

    Allowing usernames with spaces ( ) and underscores (_) to route

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -2,12 +2,34 @@ require 'spec_helper'
 
 describe 'Routes' do
   describe '/users' do
-    it 'routes when the username has periods' do
-      expect(get: '/users/alfred.e.newman').to route_to(
-        action: 'show',
-        controller: 'users',
-        id: 'alfred.e.newman'
-      )
+    context 'when the username has a period' do
+      it 'routes' do
+        expect(get: '/users/alfred.e.newman').to route_to(
+          action: 'show',
+          controller: 'users',
+          id: 'alfred.e.newman'
+        )
+      end
+    end
+
+    context 'when the username has a space' do
+      it 'routes' do
+        expect(get: '/users/james%20bond').to route_to(
+          action: 'show',
+          controller: 'users',
+          id: 'james bond'
+        )
+      end
+    end
+
+    context 'when the username has an underscore' do
+      it 'routes' do
+        expect(get: '/users/jim_henson').to route_to(
+          action: 'show',
+          controller: 'users',
+          id: 'jim_henson'
+        )
+      end
     end
   end
 end
